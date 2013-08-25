4a9f654cae348e98e7d51feb6196f119c9b32f1c
Luke Tower <lukegtower@gmail.com>
2013-08-25 15:28:48 -0400

    Fixed links on show post page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,8 @@ module ApplicationHelper
     if @post.new_record? ? 'Create Post' : 'Edit Post'
     end
   end
+
+  def fix_url(url)
+    url.starts_with?("http://") ? url : "http://#{url}"
+  end
 end
