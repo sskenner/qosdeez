ca4ccf46080052813b7e04f94b2c251b05442892
Luke Tower <lukegtower@gmail.com>
2013-08-27 18:29:01 -0400

    Added helpers for urls and date/time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,8 @@ module ApplicationHelper
   def fix_url(url)
     url.starts_with?("http://") ? url : "http://#{url}"
   end
+
+  def display_date(date)
+    date.strftime('%B %d, %Y at %I:%M %p')
+  end
 end
