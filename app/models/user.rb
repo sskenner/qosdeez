31d63330b8b2ff4b46479178df07a8770715e66b
Luke Tower <lukegtower@gmail.com>
2013-08-28 20:31:22 -0400

    Added validation of email for user creation and update

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create, length: { minimum: 3 }
+  validates :email, presence: true,
 
   after_validation :generate_slug
 
